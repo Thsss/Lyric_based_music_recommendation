83d95b72025ba1a691bc40ea8ccf71a761f9450e
Thsss <minnietang0625@gmail.com>
2020-06-02 13:03:25 +0800

    update model + dataset

--- model.py
+++ model.py
@@ -5,8 +5,8 @@ import gensim
 import pickle
 path = os.getcwd()
 
-save_model_name = 'd2v_10k.model'
-saved_path = path + "/d2v_10k.model.bin"
+save_model_name = 'd2v_del_dbow_clean.model'
+saved_path = path + "/d2v_del_dbow_clean.bin"
 
 
 def trans2vec(d2v_data):
@@ -23,14 +23,15 @@ def recom_music(lyrics, train_corpus):
     song_name = input('请输入歌曲名:')
     id = lyrics.loc[lyrics['song_name'] == song_name].index.tolist()
     doc_id = id[0]  # 获得歌曲对应index
-    inferred_docvec = model.infer_vector(train_corpus[doc_id].words)
+    # inferred_docvec = model.infer_vector(train_corpus[doc_id].words)
+    inferred_docvec = model.docvecs[doc_id]
     sim_song = model.docvecs.most_similar([inferred_docvec], topn=5)
     sim_songs = [(lyrics['song_name'].iloc[sim_song[i][0]],lyrics['singer'].iloc[sim_song[i][0]], sim_song[i][1]) for i in range(5)]
     return sim_songs
 
 
 if __name__ == '__main__':
-    lyrics = pd.read_csv(path + '/trainSet_new.csv')
+    lyrics = pd.read_csv(path + '/trainSet3_deleted.csv')
     d2v_data = pd.Series(lyrics.iloc[:, 0])
 
     train_corpus = trans2vec(d2v_data)  # 获得模型训练corpus
